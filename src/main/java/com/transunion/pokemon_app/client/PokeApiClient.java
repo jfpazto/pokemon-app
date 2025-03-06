61fa6620ef3b1eb028e0176498d864b242bb0288
jfpazto <jfpazto@hotmail.com>
2025-03-06 13:11:32 -0500

    feat: Se crean clase para conectar con el api y service

---
+++ src/main/java/com/transunion/pokemon_app/client/PokeApiClient.java
@@ -0,0 +1,35 @@
+package com.transunion.pokemon_app.client;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.transunion.pokemon_app.dto.PokemonDTO;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PokeApiClient {
+    private final RestTemplate restTemplate = new RestTemplate();
+    private static final String BASE_URL= "https://pokeapi.co/api/v2/pokemon/";
+    public PokemonDTO obtenerPokemonDesdeApi(int idPokeapi){
+        String url= BASE_URL + idPokeapi;
+        JsonNode response = restTemplate.getForObject(url, JsonNode.class);
+        if (response==null){
+            throw new RuntimeException("No se obtuvo los datos para el pokemon" + idPokeapi);
+        }
+        PokemonDTO pokemon= new PokemonDTO();
+        pokemon.setIdPokeapi(idPokeapi);
+        pokemon.setName(response.get("name").asText());
+        pokemon.setHeight(response.get("height").asInt());
+        pokemon.setWeight(response.get("weight").asInt());
+
+        List<String> tipos = new ArrayList<>();
+        response.get("types").forEach(typeNode -> tipos.add(typeNode.get("type").get("name").asText()));
+        pokemon.setTypes(tipos.toArray(new String[0]));
+
+        List<String> habilidades = new ArrayList<>();
+        response.get("abilities").forEach(abilityNode -> habilidades.add(abilityNode.get("ability").get("name").asText()));
+        pokemon.setAbilities(habilidades.toArray(new String[0]));
+
+        return pokemon;
+    }
+}
---
+++ src/main/java/com/transunion/pokemon_app/service/PokemonService.java
@@ -0,0 +1,29 @@
+package com.transunion.pokemon_app.service;
+
+import com.transunion.pokemon_app.client.PokeApiClient;
+import com.transunion.pokemon_app.dto.PokemonDTO;
+import com.transunion.pokemon_app.dto.PokemonFilterDTO;
+import com.transunion.pokemon_app.repository.PokemonRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PokemonService {
+    private final PokemonRepository pokemonRepository;
+    private final PokeApiClient pokeApiClient;
+
+    public PokemonService(PokemonRepository pokemonRepository, PokeApiClient pokeApiClient) {
+        this.pokemonRepository = pokemonRepository;
+        this.pokeApiClient = pokeApiClient;
+    }
+
+    public void sincronizarPokemonDesdeApi(int idPokeApi){
+        PokemonDTO pokemonDTO=pokeApiClient.obtenerPokemonDesdeApi(idPokeApi);
+        pokemonRepository.cargarPokemon(pokemonDTO);
+    }
+
+    public List<PokemonDTO> buscarPokemones (PokemonFilterDTO filtro){
+        return pokemonRepository.buscarPokemones(filtro);
+    }
+}
